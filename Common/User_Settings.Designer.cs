e04a87086f22f87d9ea9588c6b4fefbc7a081a0d
Zinadin Zidan <zidan.roking@gmail.com>
2023-12-25 22:33:40 +0600

    EM: Add Users settings
    * Simple settings for allowing users to change there password
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: Ia5fae4817d5c77ac61664c5b287d8f7a129a71f0

---
+++ Common/User_Settings.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ElectionApp.Common
+{
+    partial class User_Settings
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            errorProvider1 = new ErrorProvider(components);
+            errorProvider2 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(242, 218);
+            label1.Name = "label1";
+            label1.Size = new Size(247, 32);
+            label1.TabIndex = 28;
+            label1.Text = "Current Password:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(242, 301);
+            label2.Name = "label2";
+            label2.Size = new Size(209, 32);
+            label2.TabIndex = 29;
+            label2.Text = "New Password:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(242, 378);
+            label3.Name = "label3";
+            label3.Size = new Size(252, 32);
+            label3.TabIndex = 30;
+            label3.Text = "Confirm Password:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(524, 218);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(331, 32);
+            textBox1.TabIndex = 31;
+            textBox1.Validating += textBox1_Validating;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(524, 301);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(331, 32);
+            textBox2.TabIndex = 32;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(524, 378);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(331, 32);
+            textBox3.TabIndex = 33;
+            textBox3.Validating += textBox3_Validating;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(201, 48, 44);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(428, 468);
+            button1.Name = "button1";
+            button1.Size = new Size(170, 46);
+            button1.TabIndex = 34;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            errorProvider2.ContainerControl = this;
+            // 
+            // User_Settings
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "User_Settings";
+            Size = new Size(1088, 839);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private ErrorProvider errorProvider1;
+        private ErrorProvider errorProvider2;
+    }
+}
---
+++ Common/User_Settings.cs
@@ -0,0 +1,135 @@
+﻿using System.ComponentModel;
+using System.Data.SqlClient;
+
+namespace ElectionApp.Common
+{
+    public partial class User_Settings : UserControl
+    {
+        private string givenID;
+        private string connectionString;
+
+        public User_Settings(string givenID, string connectionString)
+        {
+            GivenID = givenID;
+            ConnectionString = connectionString;
+            InitializeComponent();
+        }
+
+        private string GivenID
+        {
+            get { return givenID; }
+            set { givenID = value; }
+        }
+
+        private string ConnectionString
+        {
+            get { return connectionString; }
+            set { connectionString = value; }
+        }
+
+        // Fetch the current password associated with givenID from the LOGIN table
+        private string GetCurrentPassword(string userID)
+        {
+            string currentPassword = string.Empty;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    string selectPasswordQuery = "SELECT PASSWORD FROM LOGIN WHERE UID = @UserID";
+                    SqlCommand command = new SqlCommand(selectPasswordQuery, connection);
+                    command.Parameters.AddWithValue("@UserID", userID);
+
+                    // Execute the query and get the password
+                    object result = command.ExecuteScalar();
+                    if (result != null)
+                    {
+                        currentPassword = result.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching current password: " + ex.Message);
+            }
+
+            return currentPassword;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string currentPassword = GetCurrentPassword(GivenID);
+
+            if (textBox1.Text != currentPassword)
+            {
+                errorProvider1.SetError(textBox1, "Incorrect current password.");
+            }
+            else
+            {
+                errorProvider1.Clear();
+
+                if (textBox2.Text != textBox3.Text)
+                {
+                    errorProvider2.SetError(textBox3, "Passwords do not match.");
+                }
+                else
+                {
+                    errorProvider2.Clear();
+
+                    // Update the password in the LOGIN table
+                    try
+                    {
+                        using (SqlConnection connection = new SqlConnection(ConnectionString))
+                        {
+                            connection.Open();
+
+                            string updatePasswordQuery = "UPDATE LOGIN SET PASSWORD = @NewPassword WHERE UID = @UserID";
+                            SqlCommand command = new SqlCommand(updatePasswordQuery, connection);
+                            command.Parameters.AddWithValue("@NewPassword", textBox2.Text);
+                            command.Parameters.AddWithValue("@UserID", GivenID);
+                            command.ExecuteNonQuery();
+
+                            MessageBox.Show("Password updated successfully!");
+
+                            textBox1.Clear();
+                            textBox2.Clear();
+                            textBox3.Clear();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error updating password: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            string currentPassword = GetCurrentPassword(GivenID);
+
+            if (textBox1.Text != currentPassword)
+            {
+                errorProvider1.SetError(textBox1, "Incorrect current password.");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void textBox3_Validating(object sender, CancelEventArgs e)
+        {
+            if (textBox2.Text != textBox3.Text)
+            {
+                errorProvider2.SetError(textBox3, "Passwords do not match.");
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+        }
+    }
+}
